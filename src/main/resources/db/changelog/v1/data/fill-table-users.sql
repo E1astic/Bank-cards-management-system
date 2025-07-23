988bd11330dcbc5f2aa77bf51b6abf5682b527db
Filimonov Roman <darkr2004@mailru>
2025-07-23 17:30:11 +0300

    Настроены pom.xml и docker-compose. Создана база данных в PostgreSQL, таблицы Users и Roles. Настроены liquibase-миграции в формате XML.

---
+++ src/main/resources/db/changelog/v1/data/fill-table-users.sql
@@ -0,0 +1,6 @@
+insert into users(email, password, phone, surname, name, patronymic, role_id)
+    values('admin@mail.ru', '$2a$10$SexTb3WX5/FJDbCPyLolQeG3HUrojcWwejLNbVVYuWaJ71RgwaPCe',
+           '+7777777', 'admin', 'admin', 'admin', 1);
+insert into users(email, password, phone, surname, name, patronymic, role_id)
+    values('user@mail.ru', '$2a$10$S8u4Tre0ESYf7imYimaXCupCybpCUGb9s0M1zt/JckCRXsMEFcJw6',
+       '+7777778', 'user', 'user', 'user', 2);
